1862915b39859c17d1f9f5caf7bf114f6fd35be5
Jonathan Llovet <jonathan.llovet@gmail.com>
2022-03-28 19:14:35 -0400

    Add crude daysBetweenDates and helper function

--- 002-how-to-solve-problems/calculate_age/lab.py
+++ 002-how-to-solve-problems/calculate_age/lab.py
@@ -1,27 +1,49 @@
-###
-### Define a simple nextDay procedure, that assumes
-### every month has 30 days.
-###
-### For example:
-###    nextDay(1999, 12, 30) => (2000, 1, 1)
-###    nextDay(2013, 1, 30) => (2013, 2, 1)
-###    nextDay(2012, 12, 30) => (2013, 1, 1)  (even though December really has 31 days)
-###
-
+# Define a daysBetweenDates procedure that would produce the
+# correct output if there was a correct nextDay procedure.
+#
+# Note that this will NOT produce correct outputs yet, since
+# our nextDay procedure assumes all months have 30 days
+# (hence a year is 360 days, instead of 365).
+#
 
 def nextDay(year, month, day):
-    """
-    Returns the year, month, day of the next day.
-    Simple version: assume every month has 30 days.
-    """
+    """Simple version: assume every month has 30 days"""
     if day < 30:
-        return (year, month, day + 1)
-    if day == 30:
-        if month < 12:
-            return (year, month + 1, 1)
+        return year, month, day + 1
+    else:
+        if month == 12:
+            return year + 1, 1, 1
         else:
-            return (year + 1, 1, 1)
+            return year, month + 1, 1
+
+def compareDays(year1, month1, day1, year2, month2, day2):
+    """Returns True if two days are equal, otherwise returns False"""
+    return year1 == year2 and month1 == month2 and day1 == day2
+
+def daysBetweenDates(year1, month1, day1, year2, month2, day2):
+    """Returns the number of days between year1/month1/day1
+       and year2/month2/day2. Assumes inputs are valid dates
+       in Gregorian calendar, and the first date is not after
+       the second."""
+    sameDay = compareDays(year1, month1, day1, year2, month2, day2)
+    difference = 0
+    y1, m1, d1 = year1, month1, day1
+    while not sameDay:
+        difference += 1
+        y1, m1, d1 = nextDay(y1, m1, d1)
+        sameDay = compareDays(y1, m1, d1, year2, month2, day2)
+    return difference
+
+def test():
+    test_cases = [((2012,9,30,2012,10,30),30),
+                  ((2012,1,1,2013,1,1),360),
+                  ((2012,9,1,2012,9,4),3)]
     
-print(nextDay(1999, 12, 30))
-print(nextDay(2013, 1, 30))
-print(nextDay(2012, 12, 30))
+    for (args, answer) in test_cases:
+        result = daysBetweenDates(*args)
+        if result != answer:
+            print("Test with data:", args, "failed")
+        else:
+            print("Test case passed!")
+
+test()
