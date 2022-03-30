62a48cf74ff9bd50901a51a62693396e6c8e7448
Jonathan Llovet <jonathan.llovet@gmail.com>
2022-03-29 21:02:47 -0400

    Refactor Task2 solution for clarity, with additional explanations

--- 004-unscramble-computer-science-project/Task2.py
+++ 004-unscramble-computer-science-project/Task2.py
@@ -29,31 +29,43 @@ September 2016.".
 # Since it keeps running score as it iteratesover the list of calls, only a single
 # pass is needed over the list, and it produces a solution in linear time.
 
-call_times = dict()
+# A note on the implementation: to avoid having to allocate additional
+# memory for the call_times dict in each of the helper functions, it is contained
+# within the parent scope of those helper functions and is a semi-global variable,
+# with the caveat that it is still only available within the scope of the function
+# find_number_on_phone_longest. For this to work, the helper functions are defined
+# and executed in the scope of find_number_on_phone_longest.
 
-# Initialize to first calling number encountered
-number_on_phone_longest = calls[0][0]
+def find_number_on_phone_longest():
 
-# Finds solution in single pass over list. Linear time!
-for c in calls:
-    # Handle calling telephone number
-    if call_times.get(c[0]) is not None:
-        call_times[c[0]] += int(c[3]) # Add duration of call to entry for number
-    else:
-        call_times[c[0]] = int(c[3]) # Add number to dict if not present with duration of call
-    
-    # Compare to number listed as being on the longest and switch if needed
-    if call_times[c[0]] > call_times.get(number_on_phone_longest):
-        number_on_phone_longest = c[0]
-    
-    # Handle receiving telephone number
-    if call_times.get(c[1]) is not None:
-        call_times[c[1]] += int(c[3]) # Add duration of call to entry for number
-    else:
-        call_times[c[1]] = int(c[3]) # Add number to dict if not present with duration of call
-    
-    # Compare to number listed as being on the longest and switch if needed
-    if call_times[c[1]] > call_times.get(number_on_phone_longest):
-        number_on_phone_longest = c[1]
+    call_times = dict()
 
-print(f"{number_on_phone_longest} spent the longest time, {call_times.get(number_on_phone_longest)} seconds, on the phone during September 2016.")
+    def process_record(record, phone_number_index, number_on_phone_longest):
+        update_call_times_subtotal(record, phone_number_index)
+        return update_number_on_phone_longest(record, phone_number_index, number_on_phone_longest)
+
+    def update_call_times_subtotal(record, phone_number_index):
+        if call_times.get(record[phone_number_index]) is None:
+            call_times[record[phone_number_index]] = int(record[3]) # Add number to dict if not present with duration of call
+        else:
+            call_times[record[phone_number_index]] += int(record[3]) # Add duration of call to entry for number
+
+    def update_number_on_phone_longest(record, phone_number_index, number_on_phone_longest):
+        # Compare to number listed as being on the longest and switch if needed
+        if call_times[record[phone_number_index]] > call_times.get(number_on_phone_longest):
+            number_on_phone_longest = record[phone_number_index]
+        return number_on_phone_longest
+
+    # Initialize to first calling number encountered
+    number_on_phone_longest = calls[0][0]
+
+    # Finds solution in single pass over list. Linear time!
+    for c in calls:
+        # Handle calling telephone number
+        number_on_phone_longest = process_record(c, 0, number_on_phone_longest)
+        # Handle receiving telephone number
+        number_on_phone_longest = process_record(c, 1, number_on_phone_longest)
+
+    print(f"{number_on_phone_longest} spent the longest time, {call_times.get(number_on_phone_longest)} seconds, on the phone during September 2016.")
+
+find_number_on_phone_longest()
