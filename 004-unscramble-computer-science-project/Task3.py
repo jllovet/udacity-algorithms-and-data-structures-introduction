5dd7229c3dfad094ecc024de6a487e7f9b2b3a5b
Jonathan Llovet <jonathan.llovet@gmail.com>
2022-03-29 23:41:44 -0400

    Add solution to Task3

--- 004-unscramble-computer-science-project/Task3.py
+++ 004-unscramble-computer-science-project/Task3.py
@@ -48,3 +48,94 @@ The percentage should have 2 decimal digits
 # For reference:
 # The text data (`text.csv`) has the following columns: sending telephone number (string), receiving telephone number (string), timestamp of text message (string).
 # The call data (`call.csv`) has the following columns: calling telephone number (string), receiving telephone number (string), start timestamp of telephone call (string), duration of telephone call in seconds (string)
+
+def is_bangalore_number(n):
+    return n[:5] == "(080)"
+
+def is_mobile_number(n):
+    return " " in n and n[0] in ["7", "8", "9"]
+
+def is_telemarketer_number(n):
+    return n[:3] == "140"
+
+def is_fixed_line_number(n):
+    return "(" in n and ")" in n
+
+def get_area_code(n):
+    if is_bangalore_number(n):
+        return "(080)"
+    if is_fixed_line_number(n):
+        return n[ 0 : n.index(")") + 1 ]
+    if is_telemarketer_number(n):
+        return "140"
+    if is_mobile_number(n):
+        return n[:4]
+
+class AreaCode:
+    """AreaCode represents a summary of the phone activity from an area code
+
+    Attributes:
+        area_code: str of area code
+        phone_numbers: set of strings for phone numbers in call logs with area code
+        called_number_history: list of strings for phone numbers called from within area code
+        codes_called: set of strings for area codes called from area code
+    """
+    def __init__(self, area_code, phone_numbers, called_number_history, codes_called):
+        self.area_code = area_code
+        self.phone_numbers = set(phone_numbers)
+        self.called_number_history = list(called_number_history)
+        self.codes_called = set(codes_called)
+    def get_history_of_numbers_called_in_area_code(self, code):
+        """Returns a filtered list of str for phone numbers for each call made to the area code provided
+
+        Note: this does not return a set, but rather a filtered list of the call history from the area code
+        """
+        h = []
+        for number in self.called_number_history:
+            if get_area_code(number) == code:
+                h.append(number)
+        return h
+
+def aggregate_call_logs_by_area_code():
+    """Returns a dict of AreaCode to summarize call log information
+    """
+    calls_summary = dict()
+    for c in calls:
+        calling_number = c[0]
+        receiving_number = c[1]
+        code_of_caller = get_area_code(c[0])
+        code_of_receiver = get_area_code(c[1])
+        
+        # Construct a new AreaCode if not already in the dictionary
+        if calls_summary.get(code_of_caller) is None:
+            calls_summary[code_of_caller] = AreaCode(
+                area_code=code_of_caller,
+                phone_numbers=set([calling_number]),
+                codes_called=set([code_of_receiver]),
+                called_number_history=list(receiving_number),
+            )
+        # Update the AreaCode record if it is already in the dictionary
+        else:
+            calls_summary[code_of_caller].phone_numbers.add(calling_number)
+            calls_summary[code_of_caller].codes_called.add(code_of_receiver)
+            calls_summary[code_of_caller].called_number_history.append(receiving_number)
+    return calls_summary
+
+
+def get_area_codes_called_from_bangalore(calls_summary):
+    codes = calls_summary.get("(080)").codes_called
+    print("The numbers called by people in Bangalore have codes:")
+    for n in sorted(codes):
+        print(n)
+
+def get_percentage_of_calls_within_bangalore(calls_summary):
+    area_code = "(080)"
+    b = calls_summary.get(area_code)
+    num_calls_in_same_area_code = len(b.get_history_of_numbers_called_in_area_code(area_code))
+    num_all_calls_from_area_code = len(b.called_number_history)
+    percentage = round(num_calls_in_same_area_code / num_all_calls_from_area_code * 100, 2)
+    print(f"{percentage} percent of calls from fixed lines in Bangalore are calls to other fixed lines in Bangalore.")
+
+calls_summary = aggregate_call_logs_by_area_code()
+get_area_codes_called_from_bangalore(calls_summary)
+get_percentage_of_calls_within_bangalore(calls_summary)
