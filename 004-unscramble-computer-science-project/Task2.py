c8531680937377db6cf9749130677847d96e9cef
Jonathan Llovet <jonathan.llovet@gmail.com>
2022-03-29 20:36:41 -0400

    Add solution to Task2 with commentary

--- 004-unscramble-computer-science-project/Task2.py
+++ 004-unscramble-computer-science-project/Task2.py
@@ -24,3 +24,36 @@ September 2016.".
 # The text data (`text.csv`) has the following columns: sending telephone number (string), receiving telephone number (string), timestamp of text message (string).
 # The call data (`call.csv`) has the following columns: calling telephone number (string), receiving telephone number (string), start timestamp of telephone call (string), duration of telephone call in seconds (string)
 
+# This approach keeps a dictionary of running totals of call times per number
+# and a running tally of the number that has spent the longest on the phone so far.
+# Since it keeps running score as it iteratesover the list of calls, only a single
+# pass is needed over the list, and it produces a solution in linear time.
+
+call_times = dict()
+
+# Initialize to first calling number encountered
+number_on_phone_longest = calls[0][0]
+
+# Finds solution in single pass over list. Linear time!
+for c in calls:
+    # Handle calling telephone number
+    if call_times.get(c[0]) is not None:
+        call_times[c[0]] += int(c[3]) # Add duration of call to entry for number
+    else:
+        call_times[c[0]] = int(c[3]) # Add number to dict if not present with duration of call
+    
+    # Compare to number listed as being on the longest and switch if needed
+    if call_times[c[0]] > call_times.get(number_on_phone_longest):
+        number_on_phone_longest = c[0]
+    
+    # Handle receiving telephone number
+    if call_times.get(c[1]) is not None:
+        call_times[c[1]] += int(c[3]) # Add duration of call to entry for number
+    else:
+        call_times[c[1]] = int(c[3]) # Add number to dict if not present with duration of call
+    
+    # Compare to number listed as being on the longest and switch if needed
+    if call_times[c[1]] > call_times.get(number_on_phone_longest):
+        number_on_phone_longest = c[1]
+
+print(f"{number_on_phone_longest} spent the longest time, {call_times.get(number_on_phone_longest)} seconds, on the phone during September 2016.")
