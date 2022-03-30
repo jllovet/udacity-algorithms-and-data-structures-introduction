88d8ddb725492f5b4d7a11bd27e76651f08b5e2e
Jonathan Llovet <jonathan.llovet@gmail.com>
2022-03-29 20:08:21 -0400

    Add solution to Task1

--- 004-unscramble-computer-science-project/Task1.py
+++ 004-unscramble-computer-science-project/Task1.py
@@ -18,3 +18,19 @@ How many different telephone numbers are there in the records?
 Print a message:
 "There are <count> different telephone numbers in the records."
 """
+
+# For reference:
+# The text data (`text.csv`) has the following columns: sending telephone number (string), receiving telephone number (string), timestamp of text message (string).
+# The call data (`call.csv`) has the following columns: calling telephone number (string), receiving telephone number (string), start timestamp of telephone call (string), duration of telephone call in seconds (string)
+
+numbers = set()
+
+for t in texts:
+    numbers.add(t[0])
+    numbers.add(t[1])
+
+for c in calls:
+    numbers.add(c[0])
+    numbers.add(c[1])
+
+print(f"There are {len(numbers)} different telephone numbers in the records.")
