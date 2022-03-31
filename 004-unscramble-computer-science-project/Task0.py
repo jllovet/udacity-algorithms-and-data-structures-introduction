910fb2e9260f412741e63c98e7ee99c07f1e58ba
Jonathan Llovet <jonathan.llovet@gmail.com>
2022-03-30 22:58:41 -0400

    Add analysis for solution to Task 0

--- 004-unscramble-computer-science-project/Task0.py
+++ 004-unscramble-computer-science-project/Task0.py
@@ -20,6 +20,18 @@ Print messages:
 "Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
 """
 
+# For reference:
+# The text data (`text.csv`) has the following columns: sending telephone number (string), receiving telephone number (string), timestamp of text message (string).
+# The call data (`call.csv`) has the following columns: calling telephone number (string), receiving telephone number (string), start timestamp of telephone call (string), duration of telephone call in seconds (string)
+
+"""
+Algorithmic complexity = O(1)
+
+Retrieving a specific element from an index of a list can be done in constant
+time. Performing this a constant number of times does not affect the order of
+the solutions algorithmic complexity.
+"""
+
 first_text = texts[0]
 last_call = calls[len(calls)-1]
 
