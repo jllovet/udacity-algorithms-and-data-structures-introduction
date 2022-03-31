03d30b34948b808bc3306cf744032c369ae3bfef
Jonathan Llovet <jonathan.llovet@gmail.com>
2022-03-30 22:58:27 -0400

    Add analysis for solution to Task 1

--- 004-unscramble-computer-science-project/Task1.py
+++ 004-unscramble-computer-science-project/Task1.py
@@ -23,12 +23,18 @@ Print a message:
 # The text data (`text.csv`) has the following columns: sending telephone number (string), receiving telephone number (string), timestamp of text message (string).
 # The call data (`call.csv`) has the following columns: calling telephone number (string), receiving telephone number (string), start timestamp of telephone call (string), duration of telephone call in seconds (string)
 
-# This implementation iterates over each of the lists one time and
-# adds each new phone number to the set of numbers as it proceeds.
-# Because the python built-in set has average-case retrieval of elements
-# of O(1), as we iterate over the lists of texts and calls, getting the unique
-# set of numbers can be done on average in O(t+c), where t is the number of
-# text records and c is the number of call records.
+"""
+Algorithmic complexity = O(n^2)
+
+This implementation iterates over each of the lists one time and
+adds each new phone number to the set of numbers as it proceeds.
+Because the python built-in set has average-case retrieval of elements
+of θ(1) and O(n) in the worst case, as we iterate over the lists of
+texts and calls, getting the unique set of numbers can be done on
+average in O((t+c)^2), where t is the number of text records and c is the
+number of call records. This reduces to O(n^2) for worst-case performance,
+with average performance being θ(n).
+"""
 
 numbers = set()
 
